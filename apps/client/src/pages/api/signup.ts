ad1b311dced727f99bd14eaa73b5b32cff597ffe
arnnvv <arnav.900@protonmail.com>
2023-10-10 12:42:30 +0530

    Blah

---
+++ apps/client/src/pages/api/signup.ts
@@ -0,0 +1,16 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  token: string;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  const username: string = req.body.username;
+  const password: string = req.body.password;
+
+  res.status(200).json({ token: "rahejamc" });
+}
--- apps/client/src/pages/signup.tsx
+++ apps/client/src/pages/signup.tsx
@@ -3,10 +3,14 @@ import { Signup } from "ui";
 export default function SignupPage() {
   return (
     <div>
-      <Signup onClick={(username, password)=>{
-          alert(username);
-          alert(password);
-      }}/>
+      <Signup
+        onClick={async (username, password) => {
+          const response = await axios.post("admin.kirat.com/signup", {
+            username,
+            password,
+          });
+        }}
+      />
     </div>
   );
 }
