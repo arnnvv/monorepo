ecf07752b986cdc75474242c423bf257265e3692
arnnvv <arnav.900@protonmail.com>
2023-10-11 01:01:18 +0530

    signup

--- apps/client/src/pages/api/signup.ts
+++ apps/client/src/pages/api/signup.ts
@@ -1,37 +1,44 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
-
-import {Admin} from "db";
-
-const express = require('express');
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { Admin } from "db";
+const express = require("express");
 const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 type Data = {
-  token: string;
+  token?: string;
+  message?: string;
 };
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>,
-) {
-  router.post('/signup', (req, res) => {
-    const { username, password } = req.body;
-    const admin = Admin.findOne({ username: req.user.username });
-    function callback(admin) {
-      if (admin) {
-        res.status(403).json({ message: 'Admin already exists' });
-      } else {
-        const obj = { username: username, password: password };
-        const newAdmin = new Admin(obj);
-        newAdmin.save();
+// Define your signup route outside of the handler
+router.post('/signup', (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { username, password } = req.body;
 
-        const token = jwt.sign({ username, role: 'admin' }, SECRET, { expiresIn: '1h' });
-        res.json({ message: 'Admin created successfully', token });
-      }
+  // Check if an admin with the same username already exists
+  Admin.findOne({ username }).then((admin) => {
+    if (admin) {
+      res.status(403).json({ message: 'Admin already exists' });
+    } else {
+      // Create a new admin if not already exists
+      const obj = { username, password };
+      const newAdmin = new Admin(obj);
+      newAdmin.save();
 
+      // Sign a JWT token for authentication
+      const SECRET = 'your_secret_key_here'; // Replace with your actual secret key
+      const token = jwt.sign({ username, role: 'admin' }, SECRET, { expiresIn: '1h' });
+      res.json({ message: 'Admin created successfully', token });
     }
-    Admin.findOne({ username }).then(callback);
   });
+});
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>,
+) {
   res.status(200).json({ token: "Arnav" });
 }
+
+// Use the router middleware in your application to handle the '/signup' route
+// Example usage in Next.js:
+// app.use('/api', router);
--- apps/client/src/pages/signup.tsx
+++ apps/client/src/pages/signup.tsx
@@ -1,14 +1,21 @@
 import { Signup } from "ui";
+import axios from "axios";
 
 export default function SignupPage() {
   return (
     <div>
       <Signup
-        onClick={async (username, password) => {
-          const response = await axios.post("admin.kirat.com/signup", {
-            username,
-            password,
-          });
+        onClick={async (username: string, password: string) => {
+            try {
+                const response = await axios.post("admin.kirat.com/signup", {
+                    username,
+                    password,
+                });
+            }
+            catch (err) {
+                // Handle any errors that occur during the request
+                console.error(err);
+            }
         }}
       />
     </div>
