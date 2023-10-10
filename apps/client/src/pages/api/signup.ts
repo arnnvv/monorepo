9a427008fc1a017164226170a727a3163b6510ec
arnnvv <arnav.900@protonmail.com>
2023-10-10 16:21:45 +0530

    checkpoint

--- apps/client/src/pages/api/signup.ts
+++ apps/client/src/pages/api/signup.ts
@@ -1,16 +1,37 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
+import {Admin} from "db";
+
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const router = express.Router();
+
 type Data = {
   token: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  const username: string = req.body.username;
-  const password: string = req.body.password;
+  router.post('/signup', (req, res) => {
+    const { username, password } = req.body;
+    const admin = await Admin.findOne({ username: req.user.username });
+    function callback(admin) {
+      if (admin) {
+        res.status(403).json({ message: 'Admin already exists' });
+      } else {
+        const obj = { username: username, password: password };
+        const newAdmin = new Admin(obj);
+        newAdmin.save();
 
-  res.status(200).json({ token: "rahejamc" });
+        const token = jwt.sign({ username, role: 'admin' }, SECRET, { expiresIn: '1h' });
+        res.json({ message: 'Admin created successfully', token });
+      }
+
+    }
+    Admin.findOne({ username }).then(callback);
+  });
+  res.status(200).json({ token: "Arnav" });
 }
